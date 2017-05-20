31311caeaeb4f76634d1e1c1718a4ab5be598534
Daltin Loomis <loomisdf@gmail.com>
2017-05-20 16:19:25 -0400

    add Album model

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    date_added = models.DateTimeField('date added to database')
+    last_modified = models.DateTimeField('last time updated')
+
+    # -1 is unknown
+    # 0 is free
+    # price in US dollars
+    resale_value = models.DecimalField(max_digits=19, decimal_places=2)
+    paid_value = models.DecimalField(max_digits=19, decimal_places=2)
+
+    album_name = models.CharField(max_length=200)
+    artist_name = models.CharField(max_length=200)
+    skips = models.IntegerField(default=0)
+
+    # very poor, poor, average, good, excellent, sealed
+    sleeve_quality = models.CharField(max_length=20)
+    vinyl_quality = models.CharField(max_length=20)
+
+    description = models.TextField(default="")
+
+    def __str__(self):
+        return "{0}: {1}".format(self.artist_name, self.album_name)
