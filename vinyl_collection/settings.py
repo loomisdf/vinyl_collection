4c02e756cf45804d461fd8e21b62ba00d8bd0980
Daltin Loomis <loomisdf@gmail.com>
2017-05-21 11:24:31 -0400

    add whitenoise storage

--- vinyl_collection/settings.py
+++ vinyl_collection/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
