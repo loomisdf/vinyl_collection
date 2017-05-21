28df3db9eca040dad784404904e8aad371cf4085
Daltin Loomis <loomisdf@gmail.com>
2017-05-21 15:28:28 -0400

    add page to view all records

--- collection/urls.py
+++ collection/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'collection'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^all/$', views.all, name='all'),
 ]
--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Album
 
 # Create your views here.
 
 def index(request):
     context = {}
     return render(request, 'collection/index.html', context)
+
+def all(request):
+    all_albums = Album.objects.order_by('artist_name')
+    if not all_albums:
+        raise Http404("There are no albums in the database!")
+    else:
+        context = { 'all_albums': all_albums}
+        return render(request, 'collection/all.html', context)
