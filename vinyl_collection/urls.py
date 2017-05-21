c7b85d8cab241b158c16800361606c6736f8fc50
Daltin Loomis <loomisdf@gmail.com>
2017-05-21 12:18:15 -0400

    add empty url to show index page

--- vinyl_collection/urls.py
+++ vinyl_collection/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from collection import views
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^collection/', include('collection.urls')),
     url(r'^admin/', admin.site.urls),
 ]
